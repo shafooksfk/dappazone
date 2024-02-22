9f4d99c863d2bf5e9b8743590e10d06b1fb5a47b
shafook <Muhammed.keloth>
2024-02-22 15:48:06 +0530

    feat: smart contract deployment testing

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,15 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
-
-})
+  it("check name", async () => {
+    const Dappazon = await ethers.getContractFactory("Dappazon");
+    const dappazon = await Dappazon.deploy();
+    const name = await dappazon.name();
+    expect(name).to.be.equal("Dappazon");
+  });
+});
