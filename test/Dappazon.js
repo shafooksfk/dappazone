b4f37c62beeb328fcb9bceecce1220530e02c3b5
shafook <Muhammed.keloth>
2024-02-22 17:13:06 +0530

    feat: modified test file with beforeeach

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,15 +1,25 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const {
+  Contract,
+} = require("hardhat/internal/hardhat-network/stack-traces/model");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
 describe("Dappazon", () => {
-  it("check name", async () => {
+  let dappazon;
+  beforeEach(async () => {
+    // deploy Contract
     const Dappazon = await ethers.getContractFactory("Dappazon");
-    const dappazon = await Dappazon.deploy();
-    const name = await dappazon.name();
-    expect(name).to.be.equal("Dappazon");
+    dappazon = await Dappazon.deploy();
+  });
+
+  describe("Deployment", async () => {
+    it("has a name", async () => {
+      const name = await dappazon.name();
+      expect(name).to.be.equal("Dappazon");
+    });
   });
 });
