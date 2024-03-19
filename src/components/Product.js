e022b02cadebe4317b6ec06bf6baf5ee657b3b84
shafook <Muhammed.keloth>
2024-03-19 15:24:22 +0530

    feat: ordering product

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,35 @@ import close from "../assets/close.svg";
 
 const Product = ({ item, provider, account, dappazon, togglePop }) => {
   const [order, setOrder] = useState(null);
-  const buyHandler = () => {};
+  const [hasBought, setHasBought] = useState(false);
+
+  const fetchOrderDetails = async () => {
+    const events = await dappazon.queryFilter("Buy");
+    const orders = events.filter(
+      (event) =>
+        event.args.buyer === account &&
+        event.args.itemId.toString() === item.id.toString()
+    );
+
+    if (order.length === 0) return;
+
+    const order = await dappazon.orders(account, orders[0].args.orderId);
+    setOrder(order);
+  };
+
+  const buyHandler = async () => {
+    const signer = await provider.getSigner();
+    const transaction = await dappazon
+      .connect(signer)
+      .buy(item.id, { value: item.cost });
+    await transaction.wait();
+    setHasBought(true);
+  };
+
+  useEffect(() => {
+    fetchOrderDetails();
+  }, [hasBought]);
+
   return (
     <div className="product">
       <div className="product__details">
